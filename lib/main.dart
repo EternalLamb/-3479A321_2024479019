e1bdc9ef3b1ca8b2287909f15049f3038bb9d0e3
EternalLamb <eternallamb1@gmail.com>
2026-08-25 23:56:39 -0400

    Punto 2 Arquitectura tablero

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,34 @@ class MyApp extends StatelessWidget {
       home: const Text('Hello World fellas'),
     );
   }
-
-
 }
+class PegSolitaireScreen extends StatelessWidget{
+const PegSolitaireScreen({Key? key}) : super(key : key);
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: const Text('Solitario')),
+    body: SafeArea(
+      child: Column(
+        children: [
+          Container(
+            height: 60,
+            color: Colors.grey[300],
+            child: const Center(
+              child: Text('STATUS: 349 segundos | Piezas restantes: 33',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+            ),
+          ),
+          const Divider(height: 1),
+          // Área de Juego
+          Expanded( // Expande el tablero para llenar la pantalla
+            child: Text('Tablero de Juego', style: TextStyle(fontSize: 24, color:
+            Colors.grey)),
+          ),
+        ],
+      ),
+    ),
+  );
+}}
